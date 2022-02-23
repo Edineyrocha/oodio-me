bb8acf2a464deeb2f2594e29ec7bf3e3d1c23b46
Edineyrocha <94132960+Edineyrocha@users.noreply.github.com>
2022-02-23 19:01:39 -0300

    bootcamp Java Gft

---
+++ oodio-me/src/objetosDio/Carro.java
@@ -0,0 +1,27 @@
+package objetosDio;
+
+public class Carro {
+
+	String cor;
+	String modelo;
+	int capTanque;
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getCapTanque() {
+		return capTanque;
+	}
+	public void setCapTanque(int capTanque) {
+		this.capTanque = capTanque;
+	}
+	
+}
